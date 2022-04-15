9c431cb9ac38e325b303659af15815d86937571a
Revie77 <103661436+Revie77@users.noreply.github.com>
2022-04-15 23:19:13 +0800

    Update GitTest.java

--- src/main/java/com/zzzyyy/GitTest.java
+++ src/main/java/com/zzzyyy/GitTest.java
@@ -8,8 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
-        System.out.println("hot-fix test");
-        System.out.println("push again test");
+      
 
 
     }
