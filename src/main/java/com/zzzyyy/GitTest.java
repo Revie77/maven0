94a6cd0420d48637f4027c534cceb37e524b908e
Mei <2509115745@qq.com>
2022-04-15 23:15:56 +0800

    master commit2

--- src/main/java/com/zzzyyy/GitTest.java
+++ src/main/java/com/zzzyyy/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hot-fix test");
+        System.out.println("push again test");
 
 
     }
