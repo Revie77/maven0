63e7322d7b67c76790306d48e358f482148a4aee
Mei <2509115745@qq.com>
2022-04-15 22:41:50 +0800

    hot-fix commit

--- src/main/java/com/zzzyyy/GitTest.java
+++ src/main/java/com/zzzyyy/GitTest.java
@@ -8,5 +8,10 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hot-fix test");
+
+
     }
 }
